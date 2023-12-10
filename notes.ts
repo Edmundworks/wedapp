3c8cc5188a8aa96c539fe1cc638718314d186562
punloving <edmundcuthbert@gmail.com>
2023-12-10 12:04:03 +0000

    adding more notes

---
+++ notes.ts
@@ -0,0 +1,22 @@
+/*
+
+DONT FORGET TO TSC --w when starting!
+
+how to have a picture open in a large version?
+
+make each picture a button
+onclick call a box on top? - modal
+this box also has a button
+onclick of that , close the box
+
+fix the vertical picture on page 3
+// can I pass the height in pixels as a variable?
+// interesting problem where beckbox remains checked from one page to the next
+
+// checkbox
+// on click to go forward or back, need to call a "save checkboxes" fn
+// need to reload pages with box checked using the array that fn stores
+// submit selected photos to board button
+// or just having the checkbox selected "pins" them to the board
+
+*/
